6b399445e568e77de03a0a3a4b4197720e28772c
Ryan Lewandowski <ryanlewan.2@gmail.com>
2026-06-13 17:14:55 -0500

    Init commit

---
+++ HalfLight/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HalfLight
+//
+//  Created by Ryan Lewandowski on 6/13/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, Ryan!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HalfLight/HalfLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  HalfLightApp.swift
+//  HalfLight
+//
+//  Created by Ryan Lewandowski on 6/13/26.
+//
+
+import SwiftUI
+
+@main
+struct HalfLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
